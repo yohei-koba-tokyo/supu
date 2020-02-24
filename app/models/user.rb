31d1eef7792facc62aa10b71d07760fd455b1cb2
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-02-24 20:57:18 +0900

    作戦変更。自グループのみの表示に成功したので、チャット画面を作成。フレンズテーブルは無くす？

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :group_users
-  has_many :groups, through: :group_users
+  has_many :mission_users
+  has_many :missions, through: :mission_users
 end
