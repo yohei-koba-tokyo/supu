517a94503fd808055634ba08d85a61c644013872
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-02-24 18:49:53 +0900

    mission編集

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,17 +1,40 @@
 class MissionsController < ApplicationController
 
   def index
-    @missions = Mission.all
+    @missions = Mission.all.order("datetime ASC")
   end
 
   def new
-    @missions = Mission.all
     @mission = Mission.new
-    # @mission.users << current_user
+    @mission.users << current_user
   end
 
   def create
+    @mission = Mission.new(mission_params)
+    if @mission.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
+  def edit
+    @group = Mission.find(params[:id])
+  end
+
+  def update
+    @mission = Mission.find(params[:id])
+    if @mission.update(mission_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
+
+  private
+  def mission_params
+    params.require(:mission).permit(:datetime, :name, :comment, :mission_type, user_ids: [])
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_065943) do
+ActiveRecord::Schema.define(version: 2020_02_24_053354) do
 
   create_table "mission_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "mission_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_02_23_065943) do
     t.datetime "updated_at", null: false
     t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
