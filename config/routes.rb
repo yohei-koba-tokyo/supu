7bcd6d7803c2b73da85a4a96b14ae0b199e303bc
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-02-27 11:26:45 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'missions#index'
   resources :users, only: [:index, :edit, :update]
-  resources :missions
   resources :friends
+  resources :missions
 end
