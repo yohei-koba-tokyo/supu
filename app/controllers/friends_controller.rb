edae023d83b3c14d04e740cda2a7fe7c86e3648e
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-02-27 15:59:19 +0900

    フレンドのdestroy edit 完了！　次はミッションのdestroy editを実装

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -5,6 +5,12 @@ class FriendsController < ApplicationController
   end
 
   def new
+    @friend = Friend.new
+  end
+
+  def create
+    Friend.create(friend_params)
+    redirect_to friends_path
   end
 
   def show
@@ -15,6 +21,25 @@ class FriendsController < ApplicationController
     @friend = Friend.find(params[:id])
   end
 
+  def update
+
+    friend =Friend.find(params[:id])
+    friend.update(friend_params)
+    redirect_to friends_path
+  end
+
+
+  def destroy
+    friend = Friend.find(params[:id])
+    friend.destroy
+    redirect_to friends_path
+  end
+
+
+  private
+    def friend_params
+      params.require(:friend).permit(:name, :sex, :birth, :twitter, :memo).merge(user_id: current_user.id)
+    end
 
 
 
