d11f6bb1688659e1514b0dd11d0ee39763830ba2
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-04-01 23:49:42 +0900

    controller createのテスト、不具合時のレンダー先記述

--- spec/controllers/friends_controller_spec.rb
+++ spec/controllers/friends_controller_spec.rb
@@ -1,7 +1,6 @@
 # GETは、index, new, edit, show
 
 require 'rails_helper'
-
 describe FriendsController do
 
   describe 'GET #new' do
@@ -26,13 +25,41 @@ describe FriendsController do
   end
 
   describe 'GET #index' do
-    # it "populates an array of friends" do
-    #   friends = create_list(:friend, 5)
-    #   get :index
-    #   expect(assigns(:friends)).to match(friends)
-    # end
-
     it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    it "assigns requested friend to @friend " do
+      friend = create(:friend)
+      get :show, params: { id: friend }
+      expect(assigns(:friend)).to eq friend
+    end
+    it "renders the :show template" do
+      friend = create(:friend)
+      get :show, params: { id: friend }
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'POST #create' do
+  let(:user) { create(:user) }
+    before do
+      login user
+    end
+    it "saves the new contact in the database" do
+      friend_params = attributes_for(:friend)
+      binding.pry
+      expect{
+        post :create, params: { friend: friend_params }
+      }.to change(Friend, :count).by(1)
+    end
+    it "renders the :index template" do
+      friend_params = attributes_for(:friend)
+      post :create, params: { friend: friend_params }
+      expect(response).to redirect_to friends_path
     end
   end
 
