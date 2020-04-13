d1eb0864fe04ade22c42848d3dc61dd9322d7b65
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-04-13 16:14:42 +0900

    レスポンシブサーバー全て完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'missions#index'
-  resources :users, only: %i[index edit update]
+  resources :users, only: %i[index edit update] do
+    member do
+      get 'news'
+    end
+  end
   resources :friends
   resources :missions do
     resources :messages, only: %i[index create]
