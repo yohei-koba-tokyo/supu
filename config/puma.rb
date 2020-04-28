176e57bf5cf518f7ec6871ca17a92d4f1a024a6f
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-04-28 13:28:14 +0900

    rubocopの行エラー対応

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,9 @@
-
 # 以下コメントアウトは、docker導入以前の記述
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
-
-
 
 # docker導入後の記述
 # app_root = File.expand_path("../..", __FILE__)
